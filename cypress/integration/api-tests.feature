e9bb43e2479051bdd9eb769ee0b0d6267c5470cc
Pete <petesealey.ps@gmail.com>
2022-05-05 17:53:56 +0100

    using .then and the reposnse to make expect assertions.

--- cypress/integration/api-tests.feature
+++ cypress/integration/api-tests.feature
@@ -2,7 +2,7 @@
 Feature: Google
     Scenario: POST request successful and accurate
         Given I POST new Camera data 
-        # Then I GET the new camrea data
-        # And the cameraName is a string 
+        Then I can GET the new camera data
+        #And the cameraName is a string 
         # And the Flash value is a boolean
         
--- cypress/integration/api-tests/api-tests.js
+++ cypress/integration/api-tests/api-tests.js
@@ -1,37 +1,30 @@
 import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 
-const apiEndpoint = 'https://608abf88737e470017b73d96.mockapi.io/Cameras/'
+//set the variable cameara name each time. Try and make it unique each run
+const camera_name = "camera12"
 
-const newCameraData = [
-    {
-        "createdAt": '15:43',
-        "CameraName": "Pete's New Camera",
-        "CameraLens": "6000mm",
-        "SelfTimer": false,
-        "Flash": false
-    }
-]
-Given('I POST new Camera data', () => {
-    cy.request({
-        method: 'POST',
-        url: apiEndpoint,
-        body: newCameraData    })
-// include the assertion on teh 201 status here
-    .its("status")
-    .should("eq", 201)
-});
+Given('I POST new Camera data', () =>{
+    cy.request('POST', 'https://608abf88737e470017b73d96.mockapi.io/Cameras/', { "createdAt": "today",
+                                                                                "CameraName": camera_name,
+                                                                                "CameraLens": "50mm 1.8g",
+                                                                                "SelfTimer": false,
+                                                                                "Flash": false})
+        .then((response) => {
+    // response.body is automatically serialized into JSON
+            expect(response.status).to.eq(201)
+            expect(response.body).to.have.property('CameraName',camera_name)
+           
+    })
+})
 
-
-// Then('Then I GET the new camrea data', () => {
-    
-//     });
-
-// And('Keys are all correct', ()=>{
-//     cy.request('enter the request to bring back new camera data'
-//       .its('body')
-//       .each(value => {
-//           expect(value).to.have.all.keys('createdAt','CameraName','CameraLens','SelfTimer','Flash','id')
-
-//       })
-//     )
-// })
+Then('I can GET the new camera data', () =>{
+    cy.request('GET', 'https://608abf88737e470017b73d96.mockapi.io/Cameras/?CameraName=' + camera_name)
+       .then((response) => {
+            // response.body is automatically serialized into JSON
+            expect(response.status).to.eq(200)
+            //the string assertion below worked on the post api.
+            //It appears to be that the POST returns a response in a different format
+            //to the GET
+            expect(response).to.have.property('CameraName').to.be.a('string')
+            })
+})
